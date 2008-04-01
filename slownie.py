315fb3c2166db3e6273b5a45c0fc01592b7dc7a3
Jakub Wilk <jwilk@jwilk.net>
2008-04-01 17:02:55 +0200

    Add copyright information etc.

--- slownie.py
+++ slownie.py
@@ -1,4 +1,22 @@
 # encoding=UTF-8
+#
+# Copyright © 2008 Jakub Wilk <ubanus@users.sf.net>
+#
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License, version 2, as published
+# by the Free Software Foundation.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
+# or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
+# for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, see <http://www.gnu.org/licenses>.
+
+__author__ = 'Jakub Wilk <ubanus@users.sf.net>'
+__date__ = '2008-04-01'
+__version__ = '0.hg14'
 
 __doc__ = ur'''
 Polish text representation of numbers.
