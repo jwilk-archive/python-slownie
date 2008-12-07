b60b0d80d8a9d41db38cc0e2b8cf00a37b25eaa5
Jakub Wilk <jwilk@jwilk.net>
2008-12-07 18:29:07 +0100

    Expand tabs.

--- slownie.py
+++ slownie.py
@@ -121,8 +121,8 @@ SLOWNIE_1X[0] = SLOWNIE_10X[0] = SLOWNIE_10X[1] = SLOWNIE_100X[0] = None
 PREFIXES = u'mi bi try kwadry kwinty seksty septy okty noni decy'.split()
 
 SLOWNIE_1000XX = \
-	[None, (u'tysiąc', u'tysiące', u'tysięcy')] + \
-	[(base, base + u'y', base + u'ów') for i in xrange(2 * len(PREFIXES)) for base in [PREFIXES[i >> 1] + (i & 1 and u'liard' or u'lion')]]
+    [None, (u'tysiąc', u'tysiące', u'tysięcy')] + \
+    [(base, base + u'y', base + u'ów') for i in xrange(2 * len(PREFIXES)) for base in [PREFIXES[i >> 1] + (i & 1 and u'liard' or u'lion')]]
 del i, base
 
 SLOWNIE_0 = u'zero'
@@ -131,65 +131,65 @@ UNIT_ZLOTY = tuple(u'złot' + suffix for suffix in (u'y', u'e', u'ych'))
 UNIT_GROSZ = tuple(u'grosz' + suffix for suffix in (u'y', u'e', u'y'))
 
 def inflect(i, forms):
-	if forms is None:
-		return None
-	form_1, form_2, form_5 = forms
-	if i == 1:
-		return form_1
-	i = i % 100
-	if i in (2, 3, 4) or (i > 20 and i % 10 in (2, 3, 4)):
-		return form_2
-	else:
-		return form_5
+    if forms is None:
+        return None
+    form_1, form_2, form_5 = forms
+    if i == 1:
+        return form_1
+    i = i % 100
+    if i in (2, 3, 4) or (i > 20 and i % 10 in (2, 3, 4)):
+        return form_2
+    else:
+        return form_5
 
 def slownie999(i):
-	i = int(i)
-	if i < 1:
-		raise ValueError
-	if i > 999:
-		raise ValueError
-	words = []
-	words += SLOWNIE_100X[i // 100],
-	i = i % 100
-	if i < 20:
-		words += SLOWNIE_1X[i],
-	else:
-		words += (SLOWNIE_10X[i // 10], SLOWNIE_1X[i % 10])
-	return u' '.join(word for word in words if word)
+    i = int(i)
+    if i < 1:
+        raise ValueError
+    if i > 999:
+        raise ValueError
+    words = []
+    words += SLOWNIE_100X[i // 100],
+    i = i % 100
+    if i < 20:
+        words += SLOWNIE_1X[i],
+    else:
+        words += (SLOWNIE_10X[i // 10], SLOWNIE_1X[i % 10])
+    return u' '.join(word for word in words if word)
 
 def slownie(i, jeden=False, unit=None):
 
-	i = int(i)
-	if i < 0:
-		raise ValueError('i < 0')
-	if i == 0:
-		words = [SLOWNIE_0]
-	elif i == 1:
-		words = [SLOWNIE_1X[1]]
-	else:
-		words = []
-	words += inflect(i, unit),
-	if i == 1:
-		i = 0
-	m = 0
-	while i > 0:
-		i, j = divmod(i, 1000)
-		if j > 0:
-			if j != 1 or jeden or m == 0:
-				t = slownie999(j)
-			else:
-				t = None
-			try:
-				words = [t, inflect(j, SLOWNIE_1000XX[m])] + words
-			except IndexError:
-				raise ValueError('i >= 10 ** %d' % (3 * len(SLOWNIE_1000XX)))
-		m += 1
-	return u' '.join(word for word in words if word)
+    i = int(i)
+    if i < 0:
+        raise ValueError('i < 0')
+    if i == 0:
+        words = [SLOWNIE_0]
+    elif i == 1:
+        words = [SLOWNIE_1X[1]]
+    else:
+        words = []
+    words += inflect(i, unit),
+    if i == 1:
+        i = 0
+    m = 0
+    while i > 0:
+        i, j = divmod(i, 1000)
+        if j > 0:
+            if j != 1 or jeden or m == 0:
+                t = slownie999(j)
+            else:
+                t = None
+            try:
+                words = [t, inflect(j, SLOWNIE_1000XX[m])] + words
+            except IndexError:
+                raise ValueError('i >= 10 ** %d' % (3 * len(SLOWNIE_1000XX)))
+        m += 1
+    return u' '.join(word for word in words if word)
 
 __all__ = 'slownie', 'UNIT_ZLOTY', 'UNIT_GROSZ'
 
 if __name__ == '__main__':
-	import doctest
-	doctest.testmod()
+    import doctest
+    doctest.testmod()
 
-# vim:ts=4 sw=4 noet
+# vim:ts=4 sw=4 et
