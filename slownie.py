39b744b741fdbb04d3d54f40fe8b4cad0c3e3f20
Jakub Wilk <jwilk@jwilk.net>
2008-12-09 15:18:13 +0100

    Initial Python 3.0 support.
    --HG--
    branch : py3k

--- slownie.py
+++ slownie.py
@@ -15,80 +15,80 @@
 # this program; if not, see <http://www.gnu.org/licenses>.
 
 __author__ = 'Jakub Wilk <ubanus@users.sf.net>'
-__date__ = '2008-04-01'
-__version__ = '0.hg15'
+__date__ = '2008-12-09'
+__version__ = '0.hg15.py3k'
 
-__doc__ = ur'''
+__doc__ = r'''
 Polish text representation of numbers.
 
 >>> slownie(0)
-u'zero'
+'zero'
 >>> slownie(1)
-u'jeden'
+'jeden'
 >>> slownie(2)
-u'dwa'
+'dwa'
 >>> slownie(4)
-u'cztery'
+'cztery'
 >>> slownie(8)
-u'osiem'
+'osiem'
 >>> slownie(16)
-u'szesnaście'
+'szesnaście'
 >>> slownie(17)
-u'siedemnaście'
+'siedemnaście'
 >>> slownie(23)
-u'dwadzieścia trzy'
+'dwadzieścia trzy'
 >>> slownie(32)
-u'trzydzieści dwa'
+'trzydzieści dwa'
 >>> slownie(37)
-u'trzydzieści siedem'
+'trzydzieści siedem'
 >>> slownie(42)
-u'czterdzieści dwa'
+'czterdzieści dwa'
 >>> slownie(64)
-u'sześćdziesiąt cztery'
+'sześćdziesiąt cztery'
 >>> slownie(69)
-u'sześćdziesiąt dziewięć'
+'sześćdziesiąt dziewięć'
 >>> slownie(105)
-u'sto pięć'
+'sto pięć'
 >>> slownie(128)
-u'sto dwadzieścia osiem'
+'sto dwadzieścia osiem'
 >>> slownie(256)
-u'dwieście pięćdziesiąt sześć'
+'dwieście pięćdziesiąt sześć'
 >>> slownie(512)
-u'pięćset dwanaście'
+'pięćset dwanaście'
 >>> slownie(666)
-u'sześćset sześćdziesiąt sześć'
+'sześćset sześćdziesiąt sześć'
 >>> slownie(1024)
-u'tysiąc dwadzieścia cztery'
+'tysiąc dwadzieścia cztery'
 >>> slownie(1024, jeden=True)
-u'jeden tysiąc dwadzieścia cztery'
+'jeden tysiąc dwadzieścia cztery'
 >>> slownie(1 << 11)
-u'dwa tysiące czterdzieści osiem'
+'dwa tysiące czterdzieści osiem'
 >>> slownie(1 << 12)
-u'cztery tysiące dziewięćdziesiąt sześć'
+'cztery tysiące dziewięćdziesiąt sześć'
 >>> slownie(1 << 13)
-u'osiem tysięcy sto dziewięćdziesiąt dwa'
+'osiem tysięcy sto dziewięćdziesiąt dwa'
 >>> slownie(1 << 14)
-u'szesnaście tysięcy trzysta osiemdziesiąt cztery'
+'szesnaście tysięcy trzysta osiemdziesiąt cztery'
 >>> slownie(1 << 15)
-u'trzydzieści dwa tysiące siedemset sześćdziesiąt osiem'
+'trzydzieści dwa tysiące siedemset sześćdziesiąt osiem'
 >>> slownie(1 << 16)
-u'sześćdziesiąt pięć tysięcy pięćset trzydzieści sześć'
+'sześćdziesiąt pięć tysięcy pięćset trzydzieści sześć'
 >>> slownie(1 << 17)
-u'sto trzydzieści jeden tysięcy siedemdziesiąt dwa'
+'sto trzydzieści jeden tysięcy siedemdziesiąt dwa'
 >>> slownie(1 << 18)
-u'dwieście sześćdziesiąt dwa tysiące sto czterdzieści cztery'
+'dwieście sześćdziesiąt dwa tysiące sto czterdzieści cztery'
 >>> slownie(1 << 19)
-u'pięćset dwadzieścia cztery tysiące dwieście osiemdziesiąt osiem'
+'pięćset dwadzieścia cztery tysiące dwieście osiemdziesiąt osiem'
 >>> slownie(1 << 20)
-u'milion czterdzieści osiem tysięcy pięćset siedemdziesiąt sześć'
+'milion czterdzieści osiem tysięcy pięćset siedemdziesiąt sześć'
 >>> slownie(1 << 20, jeden=True)
-u'jeden milion czterdzieści osiem tysięcy pięćset siedemdziesiąt sześć'
+'jeden milion czterdzieści osiem tysięcy pięćset siedemdziesiąt sześć'
 >>> slownie(10 ** 9)
-u'miliard'
+'miliard'
 >>> slownie(17 ** 17)
-u'osiemset dwadzieścia siedem trylionów dwieście czterdzieści biliardów dwieście sześćdziesiąt jeden bilionów osiemset osiemdziesiąt sześć miliardów trzysta trzydzieści sześć milionów siedemset sześćdziesiąt cztery tysiące sto siedemdziesiąt siedem'
+'osiemset dwadzieścia siedem trylionów dwieście czterdzieści biliardów dwieście sześćdziesiąt jeden bilionów osiemset osiemdziesiąt sześć miliardów trzysta trzydzieści sześć milionów siedemset sześćdziesiąt cztery tysiące sto siedemdziesiąt siedem'
 >>> slownie(10 ** 66 - 1)
-u'dziewięćset dziewięćdziesiąt dziewięć decyliardów dziewięćset dziewięćdziesiąt dziewięć decylionów dziewięćset dziewięćdziesiąt dziewięć noniliardów dziewięćset dziewięćdziesiąt dziewięć nonilionów dziewięćset dziewięćdziesiąt dziewięć oktyliardów dziewięćset dziewięćdziesiąt dziewięć oktylionów dziewięćset dziewięćdziesiąt dziewięć septyliardów dziewięćset dziewięćdziesiąt dziewięć septylionów dziewięćset dziewięćdziesiąt dziewięć sekstyliardów dziewięćset dziewięćdziesiąt dziewięć sekstylionów dziewięćset dziewięćdziesiąt dziewięć kwintyliardów dziewięćset dziewięćdziesiąt dziewięć kwintylionów dziewięćset dziewięćdziesiąt dziewięć kwadryliardów dziewięćset dziewięćdziesiąt dziewięć kwadrylionów dziewięćset dziewięćdziesiąt dziewięć tryliardów dziewięćset dziewięćdziesiąt dziewięć trylionów dziewięćset dziewięćdziesiąt dziewięć biliardów dziewięćset dziewięćdziesiąt dziewięć bilionów dziewięćset dziewięćdziesiąt dziewięć miliardów dziewięćset dziewięćdziesiąt dziewięć milionów dziewięćset dziewięćdziesiąt dziewięć tysięcy dziewięćset dziewięćdziesiąt dziewięć'
+'dziewięćset dziewięćdziesiąt dziewięć decyliardów dziewięćset dziewięćdziesiąt dziewięć decylionów dziewięćset dziewięćdziesiąt dziewięć noniliardów dziewięćset dziewięćdziesiąt dziewięć nonilionów dziewięćset dziewięćdziesiąt dziewięć oktyliardów dziewięćset dziewięćdziesiąt dziewięć oktylionów dziewięćset dziewięćdziesiąt dziewięć septyliardów dziewięćset dziewięćdziesiąt dziewięć septylionów dziewięćset dziewięćdziesiąt dziewięć sekstyliardów dziewięćset dziewięćdziesiąt dziewięć sekstylionów dziewięćset dziewięćdziesiąt dziewięć kwintyliardów dziewięćset dziewięćdziesiąt dziewięć kwintylionów dziewięćset dziewięćdziesiąt dziewięć kwadryliardów dziewięćset dziewięćdziesiąt dziewięć kwadrylionów dziewięćset dziewięćdziesiąt dziewięć tryliardów dziewięćset dziewięćdziesiąt dziewięć trylionów dziewięćset dziewięćdziesiąt dziewięć biliardów dziewięćset dziewięćdziesiąt dziewięć bilionów dziewięćset dziewięćdziesiąt dziewięć miliardów dziewięćset dziewięćdziesiąt dziewięć milionów dziewięćset dziewięćdziesiąt dziewięć tysięcy dziewięćset dziewięćdziesiąt dziewięć'
 
 >>> slownie(10 ** 66)
 Traceback (most recent call last):
@@ -99,36 +99,32 @@ Traceback (most recent call last):
 ...
 ValueError: i < 0
 '''
-__doc__ = '\n'.join(line.encode('unicode-escape') for line in __doc__.splitlines())
 
-SLOWNIE_1X = u'''
+SLOWNIE_1X = '''
 #        jeden      dwa       trzy       cztery      pięć       sześć      siedem       osiem       dziewięć
 dziesięć jedenaście dwanaście trzynaście czternaście piętnaście szesnaście siedemnaście osiemnaście dziewiętnaście
 '''.split()
 
-SLOWNIE_10X = u'''
+SLOWNIE_10X = '''
 # # dwadzieścia trzydzieści czterdzieści
-'''.split() + [u'%sdziesiąt' % SLOWNIE_1X[i] for i in xrange(5, 10)]
-del i
+'''.split() + ['%sdziesiąt' % SLOWNIE_1X[i] for i in range(5, 10)]
 
-SLOWNIE_100X = u'''
+SLOWNIE_100X = '''
 # sto dwieście trzysta czterysta
-'''.split() + [u'%sset' % SLOWNIE_1X[i] for i in xrange(5, 10)]
-del i
+'''.split() + ['%sset' % SLOWNIE_1X[i] for i in range(5, 10)]
 
 SLOWNIE_1X[0] = SLOWNIE_10X[0] = SLOWNIE_10X[1] = SLOWNIE_100X[0] = None
 
-PREFIXES = u'mi bi try kwadry kwinty seksty septy okty noni decy'.split()
+PREFIXES = 'mi bi try kwadry kwinty seksty septy okty noni decy'.split()
 
 SLOWNIE_1000XX = \
-    [None, (u'tysiąc', u'tysiące', u'tysięcy')] + \
-    [(base, base + u'y', base + u'ów') for i in xrange(2 * len(PREFIXES)) for base in [PREFIXES[i >> 1] + (i & 1 and u'liard' or u'lion')]]
-del i, base
+    [None, ('tysiąc', 'tysiące', 'tysięcy')] + \
+    [(base, base + 'y', base + 'ów') for i in range(2 * len(PREFIXES)) for base in [PREFIXES[i >> 1] + (i & 1 and 'liard' or 'lion')]]
 
-SLOWNIE_0 = u'zero'
+SLOWNIE_0 = 'zero'
 
-UNIT_ZLOTY = tuple(u'złot' + suffix for suffix in (u'y', u'e', u'ych'))
-UNIT_GROSZ = tuple(u'grosz' + suffix for suffix in (u'y', u'e', u'y'))
+UNIT_ZLOTY = tuple('złot' + suffix for suffix in ('y', 'e', 'ych'))
+UNIT_GROSZ = tuple('grosz' + suffix for suffix in ('y', 'e', 'y'))
 
 def inflect(i, forms):
     if forms is None:
@@ -155,7 +151,7 @@ def slownie999(i):
         words += SLOWNIE_1X[i],
     else:
         words += (SLOWNIE_10X[i // 10], SLOWNIE_1X[i % 10])
-    return u' '.join(word for word in words if word)
+    return ' '.join(word for word in words if word)
 
 def slownie(i, jeden=False, unit=None):
 
@@ -184,7 +180,7 @@ def slownie(i, jeden=False, unit=None):
             except IndexError:
                 raise ValueError('i >= 10 ** %d' % (3 * len(SLOWNIE_1000XX)))
         m += 1
-    return u' '.join(word for word in words if word)
+    return ' '.join(word for word in words if word)
 
 __all__ = 'slownie', 'UNIT_ZLOTY', 'UNIT_GROSZ'
 
